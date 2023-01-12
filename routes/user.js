8d1194b3fde37ec0196b7460b7db9fbf50a288c4
Noctern <alexsky.gt12@gmail.com>
2023-01-12 21:14:57 +0700

    fixed user authentication

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,68 @@
 import { Router } from 'express'
-import { registerUser, loginUser, getMe, apiDemo, subscribe } from '../controllers/user.js'
-import { protect } from '../middlewares/auth.js'
+import { registerUser, loginUser, logoutUser, changeUserType, removeUser,
+    subscribe, borrowBook, returnBook, editProfile, rateBook } from '../controllers/user.js'
+import { authenticate, authorizeAdmin, authorizeStaff } from '../middlewares/auth.js'
+import { getBorrowing, getBorrowed } from '../middlewares/order.js'
+import { getDatabase, approveSubscription } from '../middlewares/admin.js'
 
 const router = Router()
 
-router.post('/register', registerUser)
-router.post('/login', loginUser)
-router.get('/me', protect, getMe)
-router.get('/api-demo', apiDemo)
-router.post('/subscribe',protect, subscribe)
+router.post('/register', registerUser, function (req, res) {
+    if (req.userExists) {
+        res.render('register',
+            {
+                title: 'Register',
+                userExists: req.userExists,
+                popup: true
+            })
+    } else {
+        res.render('register-success',
+            {
+                title: 'Register',
+                popup: null
+            })
+    }
+})
+router.post('/login', loginUser, function (req, res) {
+    if (!req.userExists || req.deactivated) {
+        res.render('login',
+            {
+                title: 'Log in',
+                userExists: req.userExists ? req.userExists : null,
+                deactivated: req.deactivated ? req.deactivated : null,
+                popup: true
+            })
+    } else {
+        res.redirect('/')
+    }
+})
+router.post('/subscribe', authenticate, subscribe, function (req, res) {
+    if (req.type == '1' && req.status == true) {
+        res.redirect('/subscriptions?status=success')
+    } else if (req.type == '0' && req.status == true) {
+        res.redirect('/subscriptions')
+    } else {
+        res.send(req.message)
+    }
+})
+
+router.get('/login', authenticate, function (req, res) {
+    res.render('login',
+        {
+            title: 'Log in',
+            userExists: null,
+            deactivated: null,
+            popup: true
+        })
+})
+router.get('/register', function (req, res) {
+    res.render('register',
+        {
+            title: 'Register',
+            userExists: null,
+            popup: true
+        })
+})
+router.get('/logout', logoutUser)
+
 export { router }
