f268b82c9dc756bd02d1d631a52d4b34cbef1959
NGUYEN TRUONG AN <an.nt194724@sis.hust.edu.vn>
2023-01-12 11:39:39 +0700

    add api and rename file

--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,37 @@
 import { Router } from 'express'
-import { addBook, removeBook } from '../controllers/book.js'
+import { protect } from '../middlewares/auth.js'
+import { getTags, getBooks, getBookInfo, addBook, removeBook } from '../controllers/book.js'
 
 const router = Router()
 
-router.post('/addBook', authenticate, authorizeAdmin, authorizeStaff, function(req, res, next) {
+router.get('/', protect, getTags, getBooks, function(req, res) {
+    res.render('books',
+        {
+            title: 'Books',
+            user: req.user ? req.user : null,
+            books: req.books ? req.books : null,
+            authors: req.authors ? req.authors : null,
+            tags: req.tags? req.tags : null
+        })
+})
+
+router.get('/book-details', protect, getBookInfo, function (req, res) {
+    res.render('book-details',
+        {
+            title: 'Book Details',
+            user: req.user ? req.user : null,
+            book: req.book ? req.book : null,
+            author: req.author ? req.author : null,
+            publisher: req.publisher ? req.publisher : null,
+            tags: req.tags ? req.tags : null,
+            userRating: req.userRating? req.userRating.rating : 0,
+            current: req.current ? req.current : 0,
+            total: req.total ? req.total : 0,
+            borrowing: req.borrowing ? req.borrowing : null
+        })
+})
+
+router.post('/addBook', protect, function(req, res, next) {
     const authorized = (req.authorizedAdmin || req.authorizedStaff) ? true : false
     if (authorized) {
         next()
@@ -14,7 +42,7 @@ router.post('/addBook', authenticate, authorizeAdmin, authorizeStaff, function(r
     res.redirect('/user/admin-tools/books')
 })
 
-router.post('/removeBook', authenticate, authorizeAdmin, authorizeStaff, function(req, res, next) {
+router.post('/removeBook', protect, function(req, res, next) {
     const authorized = (req.authorizedAdmin || req.authorizedStaff) ? true : false
     if (authorized) {
         next()
