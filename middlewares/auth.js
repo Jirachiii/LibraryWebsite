4ff0c271fbc987f21ee2633804b8c7f49a92159c
raito <minhquangmori214@gmail.com>
2023-02-03 23:57:22 +0700

    adding features

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import asyncHandler from 'express-async-handler'
 import { User } from '../models/user.js'
+import { Subscription } from '../models/subscription.js'
 
 const authenticate = asyncHandler(async (req, res, next) => {
   if (req.cookies.access_token) {
@@ -14,6 +15,36 @@ const authenticate = asyncHandler(async (req, res, next) => {
     } catch (error) {
       res.clearCookie('access_token')
     }
+  }
+
+  next()
+})
+
+const authorizeAdmin = asyncHandler(async (req, res, next) => {
+  if (req.user && req.user.type == "admin") {
+    req.authorizedAdmin = true
+  } else {
+    req.authorizedAdmin = false
+  }
+
+  next()
+})
+
+const authorizeStaff = asyncHandler(async (req, res, next) => {
+  if (req.user && req.user.type == "staff") {
+    req.authorizedStaff = true
+  } else {
+    req.authorizedStaff = false
+  }
+
+  next()
+})
+
+const authorizeVIP = asyncHandler(async (req, res, next) => {
+  if (req.subscription && req.subscription.name == "VIP") {
+    req.authorizedVIP = true
+  } else {
+    req.authorizedVIP = false
   }
 
   next()
